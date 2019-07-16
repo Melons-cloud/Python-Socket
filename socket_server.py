88e1f902f8df3dbebc507d704205e075893018ad
hjl19910901 <18710360575@163.com>
2019-07-16 23:10:31 +0800

    Socket编程

--- socket_server.py
+++ socket_server.py
@@ -10,6 +10,14 @@ sk.bind(ip_port)
 #最大连接数
 sk.listen(5)
 #提示信息
-print("")
+print("正在进行等待接收数据......")
 #接收数据
 conn,address=sk.accept()
+#定义信息
+msg="Hello World!"
+#返回信息
+#Python3.x以上，网络数据的发送接收都是byte类型
+#如果发送的数据是str型，则需要进行编码
+conn.send(msg.encode())
+#主动关闭
+conn.close()
